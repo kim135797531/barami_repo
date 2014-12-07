dfa92168eaf423f7a2469746f1729e4d0aefe89c
Dong Min Kim <kim135797531@naver.com>
2014-12-08 03:33:38 +0900

    2

--- week01/src/com/example/week01/MainActivity.java
+++ week01/src/com/example/week01/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.activity_main2);
 		
 		//hehehehe hohoho change change
 	}
